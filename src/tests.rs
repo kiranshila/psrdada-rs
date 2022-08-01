b2aa649168d28ba21ea3eddd8da91779bc963a09
Kiran Shila <me@kiranshila.com>
2022-08-01 11:32:41 -0700

    Start test index higher (just in case)

--- src/tests.rs
+++ src/tests.rs
@@ -3,7 +3,7 @@
 
 use std::sync::atomic::AtomicI32;
 
-static TEST_BUF_IDX: AtomicI32 = AtomicI32::new(2);
+static TEST_BUF_IDX: AtomicI32 = AtomicI32::new(100);
 
 // Make sure the key we get in the tests is unique
 // We increment by 2 because the header and data buffers are both keyed, one idx apart and we alloc them together.
