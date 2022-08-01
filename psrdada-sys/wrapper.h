ff1ed23cb8bcc1c6758b68c90f66d37b67ec9a45
Kiran Shila <me@kiranshila.com>
2022-08-01 10:28:39 -0700

    ignore wrapper (because we're generating it)

--- psrdada-sys/wrapper.h
+++
@@ -1,7 +0,0 @@
-
-        #include "vendor/src/dada_client.h"
-        #include "vendor/src/dada_def.h"
-        #include "vendor/src/dada_hdu.h"
-        #include "vendor/src/dada_msg.h"
-        #include "vendor/src/ipcbuf.h"
-        
